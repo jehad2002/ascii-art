2127b63f59c04d054de24b641071aa4fb073e688
jealami <jehadalami11@gmail.com>
2023-12-05 18:49:04 +0200

    abd

--- test.go
+++ test.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"sort"
 	"strings"
 )
 
@@ -15,38 +14,26 @@ func String(text string) string {
 	chars := []byte(text)
 	s := getArt(chars[0])
 	for i := 1; i < len(chars); i++ {
-		s = fuse(s, getArt(chars[i]))
+		s = horizontalMerge(s, getArt(chars[i]))
 	}
 
 	return strings.Replace(strings.Join(s, "\n"), "$", " ", -1)
 }
 
-func fuse(left, right []string) []string {
-	var step []int
+func horizontalMerge(left, right []string) []string {
+	var result []string
 
-	for j := 0; j < 6; j++ {
-		k := len(left[j]) + len(right[j]) -
-			len(strings.TrimRight(left[j], " ")) -
-			len(strings.TrimLeft(right[j], " "))
-		step = append(step, k)
+	minLength := len(left)
+	if len(right) < minLength {
+		minLength = len(right)
 	}
 
-	sort.Ints(step)
-	c := step[0]
-	for j := 0; j < 6; j++ {
-		cc := ""
-		for i := 1; i <= c; i++ {
-			lc := left[j][len(left[j])-(c-i+1) : len(left[j])-(c-i)]
-			rc := right[j][i-1 : i]
-			if lc == " " {
-				cc += rc
-			} else {
-				cc += lc
-			}
-		}
-		left[j] = left[j][:len(left[j])-c] + cc + right[j][c:]
+	for i := 0; i < minLength; i++ {
+		mergedString := left[i] + right[i]
+		result = append(result, mergedString)
 	}
-	return left
+
+	return result
 }
 
 func main() {
